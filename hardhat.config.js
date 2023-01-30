3a2c8f1fd2d1d4d0c855744fbdca395c2d49ba12
Danishlynx <69537135+Danishlynx@users.noreply.github.com>
2023-01-30 22:56:21 +0800

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+const Volta_PRIVATE_KEY = "1360ed6b3f0b509ccc3961e2c09caccd2a9b9422101938fb238ad5326323b11a";
+
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    Volta: {
+      url: "https://volta-rpc.energyweb.org",
+      chainId: 73799,
+      accounts: [Volta_PRIVATE_KEY]
+    }
+  }
+};
