50d389749e4d2e85a0581d438edc9abd061b7172
Danishlynx <69537135+Danishlynx@users.noreply.github.com>
2023-01-30 23:54:14 +0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,14 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+const Volta_PRIVATE_KEY = "1360ed6b3f0b509ccc3961e2c09caccd2a9b9422101938fb238ad5326323b11a";
+
 module.exports = {
+  solidity: "0.8.9",
   networks: {
-    volta: {
-      url: "http://localhost:8545",
-      accounts: [
-        {
-          mnemonic: "shoulder february autumn school sorry ignore immense hurry legal acquire duck gravity",
-          balance: "100000000000000000000"
-        }
-      ]
+    Volta: {
+      url: "https://volta-rpc.energyweb.org",
+      chainId: 73799,
+      accounts: [Volta_PRIVATE_KEY]
     }
   }
 };
