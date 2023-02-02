3c280b593cea6f0bb53a40f80ea12031d8734fb7
Danishlynx <69537135+Danishlynx@users.noreply.github.com>
2023-02-02 20:25:42 +0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,8 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 const Volta_PRIVATE_KEY = "1360ed6b3f0b509ccc3961e2c09caccd2a9b9422101938fb238ad5326323b11a";
-const ALCHEMY_API_KEY = "4oh8YPsDTCF0e06AkXP_u7Fdh8-0QcIJ";
-const GOERLI_PRIVATE_KEY = "1360ed6b3f0b509ccc3961e2c09caccd2a9b9422101938fb238ad5326323b11a";
+
+
 
 module.exports = {
   solidity: "0.8.9",
@@ -12,10 +12,7 @@ module.exports = {
       chainId: 73799,
       accounts: [Volta_PRIVATE_KEY]
     }
-    goerli: {
-      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-      accounts: [GOERLI_PRIVATE_KEY]
-    }
+    
     
   }
 };
