ccbec18fd7c1358bcdabac1804f28c2b1cebf43a
Danish Ali <69537135+Danishlynx@users.noreply.github.com>
2023-02-19 14:36:08 +0800

    Update deployProxy.js

--- scripts/deployProxy.js
+++ scripts/deployProxy.js
@@ -2,7 +2,7 @@ const { ethers, upgrades } = require("hardhat");
 
 async function main() {
   const Original = await ethers.getContractFactory("Original");
-  const proxy = await upgrades.deployProxy(BoxV1, [12, 12]);
+  const proxy = await upgrades.deployProxy(Original, [12, 12]);
   await proxy.deployed();
 
   console.log(proxy.address);
