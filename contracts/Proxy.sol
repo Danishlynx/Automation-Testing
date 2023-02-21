623679ec863f4f05b485dac16b76e60feef303b5
Danish Ali <69537135+Danishlynx@users.noreply.github.com>
2023-02-21 13:50:24 +0800

    Update Proxy.sol

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/ERC1967/ERC1967Proxy.sol";
 
-contract Proxy is Initializable {
+contract Proxy is Initializable, ERC1967Proxy {
   function initialize(address _logic, bytes memory _data) public payable initializer {
-    _init(_logic, _data);
+    __ERC1967Proxy_init(_logic, _data);
   }
 }
