c652d33e94cd98b48f6029d3d4dbc3bc2129a17f
Danish Ali <69537135+Danishlynx@users.noreply.github.com>
2023-02-21 14:43:13 +0800

    Update Upgrade.js

--- scripts/Upgrade.js
+++ scripts/Upgrade.js
@@ -1,7 +1,7 @@
 const { ethers, upgrades } = require("hardhat");
 
 // TO DO: Place the address of your proxy here!
-const proxyAddress = "0x0B3230753B182C8e1BeaC76ca8C9060a1f1e807e";
+const proxyAddress = "0xD0fA67a87E73Be62D27C0A48554c7b05bc765400";
 
 async function main() {
   const Upgrade = await ethers.getContractFactory("Upgrade");
