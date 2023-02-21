4dbe33fd48c89a375d6df5067b0f5e62863bf00c
Danish Ali <69537135+Danishlynx@users.noreply.github.com>
2023-02-21 14:05:25 +0800

    Update deploy_Proxy_proxyAdmin.js

--- scripts/deploy_Proxy_proxyAdmin.js
+++ scripts/deploy_Proxy_proxyAdmin.js
@@ -7,13 +7,11 @@ async function main() {
   console.log("ProxyAdmin deployed to:", proxyAdmin.address);
 
   const Original = await ethers.getContractFactory("Original");
-  const originalAddress = "0x11e5f6854b08c3234973d9e17Ae76E9C75376778"; // replace with your deployed Original contract address
+  const originalAddress = "0xD0fA67a87E73Be62D27C0A48554c7b05bc765400"; // replace with your deployed Original contract address
 
-  const Proxy = await ethers.getContractFactory("Proxy");
-  const proxy = await Proxy.deploy(originalAddress, proxyAdmin.address);
-  await proxy.deployed();
-
-  console.log("Proxy deployed to:", proxy.address);
+  const Proxy = await ethers.getContractAt("Proxy", originalAddress);
+  await Proxy.connect(proxyAdmin.address);
+  console.log("Proxy connected to ProxyAdmin:", Proxy.address);
 }
 
 main();
