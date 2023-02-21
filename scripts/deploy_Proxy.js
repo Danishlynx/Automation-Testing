4e742de2a5d9fc943792a3c987cb38a9b56fd58c
Danish Ali <69537135+Danishlynx@users.noreply.github.com>
2023-02-21 11:33:38 +0800

    Update deploy_Proxy.js

--- scripts/deploy_Proxy.js
+++ scripts/deploy_Proxy.js
@@ -1,11 +1,19 @@
-const { ethers, upgrades } = require("hardhat");
+const { ethers } = require("hardhat");
 
 async function main() {
+  const ProxyAdmin = await ethers.getContractFactory("ProxyAdmin");
+  const proxyAdmin = await ProxyAdmin.deploy();
+  await proxyAdmin.deployed();
+  console.log("ProxyAdmin deployed to:", proxyAdmin.address);
+
   const Original = await ethers.getContractFactory("Original");
-  const proxy = await upgrades.deployProxy(Original, [12, 12]);
+  const originalAddress = "0x02500d8cfD6F57D482584881a0F3Bc48def7E16d"; // replace with your deployed Original contract address
+
+  const Proxy = await ethers.getContractFactory("Proxy");
+  const proxy = await Proxy.deploy(originalAddress, proxyAdmin.address);
   await proxy.deployed();
 
-  console.log(proxy.address);
+  console.log("Proxy deployed to:", proxy.address);
 }
 
 main();
