0a733c8d9519f5a23169d9ba0e1cf1a546e6ea5f
Danish Ali <69537135+Danishlynx@users.noreply.github.com>
2023-02-21 12:11:09 +0800

    Update Proxy.sol

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -3,8 +3,6 @@ pragma solidity ^0.8.0;
 
 
 
-
-
 contract MyProxy is TransparentUpgradeableProxy {
     constructor(address _logic, address _admin, bytes memory _data) 
         TransparentUpgradeableProxy(_logic, _admin, _data) 
