835e35a210935d5a41cfa63da9f2f18406878f55
Danish Ali <69537135+Danishlynx@users.noreply.github.com>
2023-02-21 15:03:51 +0800

    Update deploy_Upgrade.js

--- scripts/deploy_Upgrade.js
+++ scripts/deploy_Upgrade.js
@@ -1,18 +1,11 @@
-const { ethers, upgrades } = require("hardhat");
-
-// TO DO: Place the address of your proxy here!
-const proxyAddress = "0x48cf3aFC4Fb98Dbd57f33602b655E4A37C7D1c91";
+const { ethers } = require("hardhat");
 
 async function main() {
   const Upgrade = await ethers.getContractFactory("Upgrade");
-  const upgraded = await upgrades.upgradeProxy(proxyAddress, Upgrade);
-  console.log((await upgraded.area()).toString());
-  console.log((await upgraded.perimeter()).toString());
+  const upgrade = await Upgrade.deploy(12, 12);
+  await upgrade.deployed();
+
+  console.log("Upgrade deployed to:", upgrade.address);
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+main();
