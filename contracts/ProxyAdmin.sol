1e1d69efa9dc980a2301f5218fc8c8db5ef50844
Danish Ali <69537135+Danishlynx@users.noreply.github.com>
2023-02-21 13:27:38 +0800

    Delete ProxyAdmin.sol

--- contracts/ProxyAdmin.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxyAdmin.sol";
-
-contract MyProxyAdmin is TransparentUpgradeableProxyAdmin {}
