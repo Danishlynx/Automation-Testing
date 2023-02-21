f94eed13e9ead8dbfd37016174752328c0960c96
Danish Ali <69537135+Danishlynx@users.noreply.github.com>
2023-02-21 12:22:02 +0800

    Update Proxy.sol

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
 contract MyProxy is TransparentUpgradeableProxy {
     constructor(address _logic, address _admin, bytes memory _data) 
