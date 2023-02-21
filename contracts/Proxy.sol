f8b62ce65c638a59dbae88debd970919b0da7c00
Danish Ali <69537135+Danishlynx@users.noreply.github.com>
2023-02-21 13:53:58 +0800

    Update Proxy.sol

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -1,11 +1,25 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/ERC1967/ERC1967Proxy.sol";
+contract Proxy {
+    fallback() external payable {
+        address _impl = implementation();
+        assembly {
+            let ptr := mload(0x40)
+            calldatacopy(ptr, 0, calldatasize())
+            let result := delegatecall(gas(), _impl, ptr, calldatasize(), 0, 0)
+            let size := returndatasize()
+            returndatacopy(ptr, 0, size)
 
-contract Proxy is Initializable, ERC1967Proxy {
-  function initialize(address _logic, bytes memory _data) public payable initializer {
-    __ERC1967Proxy_init(_logic, _data);
-  }
+            switch result
+            case 0 { revert(ptr, size) }
+            default { return(ptr, size) }
+        }
+    }
+
+    function implementation() public view returns (address);
+
+    function upgradeTo(address _impl) public virtual;
+
+    function upgradeToAndCall(address _impl, bytes memory _data) public payable virtual;
 }
