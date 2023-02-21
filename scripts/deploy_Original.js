a7f110d56682a036a85aa80419a3818f7eaa98f1
Danish Ali <69537135+Danishlynx@users.noreply.github.com>
2023-02-21 11:23:20 +0800

    Update deploy_Original.js

--- scripts/deploy_Original.js
+++ scripts/deploy_Original.js
@@ -2,11 +2,8 @@ const { ethers } = require("hardhat");
 
 async function main() {
   const Original = await ethers.getContractFactory("Original");
-  const original = await Original.deploy();
+  const original = await Original.deploy(12, 12);
   await original.deployed();
-
-  // initialize the length and width values using 12 as the arguments
-  await original.initialize(12, 12);
 
   console.log("Original deployed to:", original.address);
 }
