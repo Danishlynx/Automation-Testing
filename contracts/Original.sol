1bffe61f0478e844592de0ed7377f6a318734b68
Danishlynx <danishlynx@gmail.com>
2023-02-21 10:37:27 +0800

    Testing

--- contracts/Original.sol
+++ contracts/Original.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract Original is Initializable {
+contract original is Initializable {
   // these state variables and their values
   // will be preserved forever, regardless of upgrading
   uint public width;
---
+++ scripts/deploy.js
@@ -0,0 +1,21 @@
+async function main() {
+    const [deployer] = await ethers.getSigners();
+  
+    console.log("Deploying contracts with the account:", deployer.address);
+  
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+  
+    const Original = await ethers.getContractFactory("Original");
+    const original = await Original.deploy();
+  
+    console.log("Original contract address:", original.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+
+
