eaf4f3df7bfe54d5ce9dbb5ac5601e89da06f3ee
Danish Ali <69537135+Danishlynx@users.noreply.github.com>
2023-02-21 12:24:57 +0800

    Update ProxyAdmin.sol

--- contracts/ProxyAdmin.sol
+++ contracts/ProxyAdmin.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxyAdmin.sol";
 
-contract MyProxyAdmin is ProxyAdmin {}
+contract MyProxyAdmin is TransparentUpgradeableProxyAdmin {}
