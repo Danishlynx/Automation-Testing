b79b6fcf72b2fb3dd5c8e5f368218a35ee7a7c64
Danish Ali <69537135+Danishlynx@users.noreply.github.com>
2023-02-23 12:01:55 +0800

    Update deploy_Proxy_proxyAdmin.js

--- scripts/deploy_Proxy_proxyAdmin.js
+++ scripts/deploy_Proxy_proxyAdmin.js
@@ -1,24 +1,31 @@
-const { ethers } = require("hardhat");
+const { ethers, upgrades } = require("hardhat");
 
 async function main() {
+  // Deploy the ProxyAdmin contract
   const ProxyAdmin = await ethers.getContractFactory("ProxyAdmin");
   const proxyAdmin = await ProxyAdmin.deploy();
   await proxyAdmin.deployed();
   console.log("ProxyAdmin deployed to:", proxyAdmin.address);
 
-  const Upgrade = await ethers.getContractFactory("Upgrade");
-  const upgrade = await Upgrade.deploy();
-  await upgrade.deployed();
-  console.log("Upgrade deployed to:", upgrade.address);
+  // Get the Original contract instance
+  const originalAddress = "0xa58187D5B92D07F53e0de434fA4c6Cd287bE084D"; // replace with your deployed Original contract address
+  const Original = await ethers.getContractAt("Original", originalAddress);
 
+  // Deploy the Proxy contract and point it to the Original contract
   const Proxy = await ethers.getContractFactory("Proxy");
-  const initData = upgrade.interface.encodeFunctionData("initialize");
-  const proxy = await upgrades.deployProxy(Proxy, upgrade.address, initData, { initializer: 'initialize' });
+  const initData = Original.interface.encodeFunctionData("initialize");
+  const proxy = await upgrades.deployProxy(Proxy, originalAddress, initData, { initializer: 'initialize' });
   await proxy.deployed();
   console.log("Proxy deployed to:", proxy.address);
 
+  // Set the ProxyAdmin contract as the admin for the Proxy contract
   await proxyAdmin.setProxyAdmin(proxy.address, proxyAdmin.address);
   console.log("ProxyAdmin set as admin of Proxy.");
 }
 
-main();
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
