35c5a82abaa3c6b2ff3aefa7726f087284d1fa6a
Danish Ali <69537135+Danishlynx@users.noreply.github.com>
2023-02-23 12:34:19 +0800

    Update deploy_Proxy_proxyAdmin.js

--- scripts/deploy_Proxy_proxyAdmin.js
+++ scripts/deploy_Proxy_proxyAdmin.js
@@ -1,29 +1,34 @@
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
 async function main() {
-  // Deploy ProxyAdmin contract
-  const proxyAdminFactory = await hre.ethers.getContractFactory("ProxyAdmin");
-  const proxyAdmin = await proxyAdminFactory.deploy();
+  const originalAddress = "0xa58187D5B92D07F53e0de434fA4c6Cd287bE084D"; // Set the address of the already deployed Original contract
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying ProxyAdmin with the account:", deployer.address);
+
+  const ProxyAdmin = await ethers.getContractFactory("contracts/proxy/ProxyAdmin.sol:ProxyAdmin");
+  const proxyAdmin = await ProxyAdmin.deploy();
   await proxyAdmin.deployed();
+
   console.log("ProxyAdmin deployed to:", proxyAdmin.address);
 
-  // Set up the TransparentUpgradeableProxy contract deployment
-  const originalAddress = "0xa58187D5B92D07F53e0de434fA4c6Cd287bE084D"; // Replace with the address of the deployed Original contract
-  const proxyFactory = await hre.ethers.getContractFactory("Proxy");
-  const initData = "";
-  const proxy = await proxyFactory.deploy(originalAddress, proxyAdmin.address, initData);
-  await proxy.deployed();
-  console.log("Proxy deployed to:", proxy.address);
+  console.log("Deploying TransparentUpgradeableProxy with the account:", deployer.address);
 
-  // Set the ProxyAdmin contract as the admin of the proxy
-  await hre.ethers.getContractAt("Proxy", proxy.address)
-    .then((proxyContract) => proxyContract.changeAdmin(proxyAdmin.address));
-  console.log("Proxy admin set to:", proxyAdmin.address);
+  const TransparentUpgradeableProxy = await ethers.getContractFactory("contracts/proxy/TransparentUpgradeableProxy.sol:TransparentUpgradeableProxy");
+  const proxy = await TransparentUpgradeableProxy.deploy(originalAddress, proxyAdmin.address, "0x");
+  await proxy.deployed();
+
+  console.log("TransparentUpgradeableProxy deployed to:", proxy.address);
+
+  console.log("Setting the admin of the Proxy to the ProxyAdmin with the account:", deployer.address);
+
+  await proxyAdmin.setProxyAdmin(proxy.address, { from: deployer.address });
+  console.log("Proxy admin set as the admin of the proxy");
+
 }
 
 main()
   .then(() => process.exit(0))
-  .catch(error => {
+  .catch((error) => {
     console.error(error);
     process.exit(1);
   });
