7f54493e32bb66f42c659a347c0f589e213112f7
Danish Ali <69537135+Danishlynx@users.noreply.github.com>
2023-02-23 14:34:30 +0800

    Update MyProxyAdmin.sol

--- contracts/MyProxyAdmin.sol
+++ contracts/MyProxyAdmin.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts/proxy/ProxyAdmin.sol";
+import "@openzeppelin/contracts/proxy/MyProxyAdmin.sol";
 
 
 contract MyProxyAdmin is ProxyAdmin, Initializable {}
