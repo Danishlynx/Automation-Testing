45e5e1c98c0d06b265bf7a0aeefae3bb7e6bbd43
Danish Ali <69537135+Danishlynx@users.noreply.github.com>
2023-02-23 12:32:58 +0800

    Update and rename Proxy.sol to TransparentUpgradeableProxy.sol

--- contracts/TransparentUpgradeableProxy.sol
+++ contracts/TransparentUpgradeableProxy.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
-contract Proxy is TransparentUpgradeableProxy {
+contract TransparentUpgradeableProxy is TransparentUpgradeableProxy {
     constructor(address _logic, address _admin, bytes memory _data) 
         TransparentUpgradeableProxy(_logic, _admin, _data) 
     {}
