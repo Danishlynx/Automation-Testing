cc2b7575f826eec3f8c904f6781dc3522fcc292e
Danish Ali <69537135+Danishlynx@users.noreply.github.com>
2023-02-23 12:28:36 +0800

    Update Proxy.sol

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
-contract MyProxy is TransparentUpgradeableProxy {
+contract Proxy is TransparentUpgradeableProxy {
     constructor(address _logic, address _admin, bytes memory _data) 
         TransparentUpgradeableProxy(_logic, _admin, _data) 
     {}
