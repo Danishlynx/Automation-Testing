a672786b5efc62c7e981333340d5ea6b32a09f88
Danish Ali <69537135+Danishlynx@users.noreply.github.com>
2023-02-23 14:17:46 +0800

    Update deploy_Proxy_proxyAdmin.js

--- scripts/deploy_Proxy_proxyAdmin.js
+++ scripts/deploy_Proxy_proxyAdmin.js
@@ -1,29 +1,29 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-  const originalAddress = "0xa58187D5B92D07F53e0de434fA4c6Cd287bE084D"; // Set the address of the already deployed Original contract
-  const [deployer] = await ethers.getSigners();
-  console.log("Deploying ProxyAdmin with the account:", deployer.address);
+  const MyProxyAdmin = await ethers.getContractFactory("MyProxyAdmin");
+  const MyTransparentUpgradeableProxy = await ethers.getContractFactory("MyTransparentUpgradeableProxy");
 
-  const ProxyAdmin = await ethers.getContractFactory("contracts/proxy/ProxyAdmin.sol:ProxyAdmin");
-  const proxyAdmin = await ProxyAdmin.deploy();
+  // Deploy the implementation contract
+  const implementation = "0xa58187D5B92D07F53e0de434fA4c6Cd287bE084D"; // Replace with the implementation contract address
+
+  // Deploy the proxy admin contract
+  const proxyAdmin = await MyProxyAdmin.deploy();
   await proxyAdmin.deployed();
-
   console.log("ProxyAdmin deployed to:", proxyAdmin.address);
 
-  console.log("Deploying TransparentUpgradeableProxy with the account:", deployer.address);
-
-  const TransparentUpgradeableProxy = await ethers.getContractFactory("contracts/proxy/TransparentUpgradeableProxy.sol:TransparentUpgradeableProxy");
-  const proxy = await TransparentUpgradeableProxy.deploy(originalAddress, proxyAdmin.address, "0x");
+  // Deploy the transparent upgradeable proxy contract
+  const proxy = await MyTransparentUpgradeableProxy.deploy(
+    implementation,
+    proxyAdmin.address,
+    "0x"
+  );
   await proxy.deployed();
-
   console.log("TransparentUpgradeableProxy deployed to:", proxy.address);
 
-  console.log("Setting the admin of the Proxy to the ProxyAdmin with the account:", deployer.address);
-
-  await proxyAdmin.setProxyAdmin(proxy.address, { from: deployer.address });
-  console.log("Proxy admin set as the admin of the proxy");
-
+  // Set the admin of the proxy contract to the proxy admin contract
+  await proxyAdmin.setProxyAdmin(proxy.address);
+  console.log("ProxyAdmin set as admin of TransparentUpgradeableProxy");
 }
 
 main()
