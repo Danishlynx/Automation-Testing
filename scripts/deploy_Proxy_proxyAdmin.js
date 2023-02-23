9dcdcb12f01bd2eed46ed47460cc822ed52e632f
Danishlynx <danishlynx@gmail.com>
2023-02-23 18:09:42 +0800

    Testing

--- scripts/deploy_Proxy_proxyAdmin.js
+++ scripts/deploy_Proxy_proxyAdmin.js
@@ -1,32 +1,17 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-  const MyProxyAdmin = await ethers.getContractFactory("ProxyAdmin");
-  const MyTransparentUpgradeableProxy = await ethers.getContractFactory("MyTransparentUpgradeableProxy");
-  const implementationAddress = "0x978eCCBF2Aaa29DFd274252C8B4Cc62F28dA5FCC"; // replace with your deployed implementation contract address
-
-  // Deploy the proxy admin contract
-  const proxyAdmin = await MyProxyAdmin.deploy();
+  const ProxyAdmin = await ethers.getContractFactory("ProxyAdmin");
+  const proxyAdmin = await ProxyAdmin.deploy();
   await proxyAdmin.deployed();
   console.log("ProxyAdmin deployed to:", proxyAdmin.address);
 
-  // Deploy the transparent upgradeable proxy contract
-  const proxy = await MyTransparentUpgradeableProxy.deploy(
-    implementationAddress,
-    proxyAdmin.address,
-    "0x"
-  );
-  await proxy.deployed();
-  console.log("TransparentUpgradeableProxy deployed to:", proxy.address);
+  const Original = await ethers.getContractFactory("Original");
+  const originalAddress = "0x48cf3aFC4Fb98Dbd57f33602b655E4A37C7D1c91"; // replace with your deployed Original contract address
 
-  // Set the admin of the proxy contract to the proxy admin contract
-  await proxyAdmin.setProxyAdmin(proxy.address);
-  console.log("ProxyAdmin set as admin of TransparentUpgradeableProxy");
+  const Proxy = await ethers.getContractAt("Proxy", originalAddress);
+  await Proxy.connect(proxyAdmin.address);
+  console.log("Proxy connected to ProxyAdmin:", Proxy.address);
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+main();
