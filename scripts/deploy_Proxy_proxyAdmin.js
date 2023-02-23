f2427c4df0a6c2f7ed444158b49847a30e19faf3
Danish Ali <69537135+Danishlynx@users.noreply.github.com>
2023-02-23 12:29:54 +0800

    Update deploy_Proxy_proxyAdmin.js

--- scripts/deploy_Proxy_proxyAdmin.js
+++ scripts/deploy_Proxy_proxyAdmin.js
@@ -9,14 +9,14 @@ async function main() {
 
   // Set up the TransparentUpgradeableProxy contract deployment
   const originalAddress = "0xa58187D5B92D07F53e0de434fA4c6Cd287bE084D"; // Replace with the address of the deployed Original contract
-  const proxyFactory = await hre.ethers.getContractFactory("TransparentUpgradeableProxy");
+  const proxyFactory = await hre.ethers.getContractFactory("Proxy");
   const initData = "";
   const proxy = await proxyFactory.deploy(originalAddress, proxyAdmin.address, initData);
   await proxy.deployed();
   console.log("Proxy deployed to:", proxy.address);
 
   // Set the ProxyAdmin contract as the admin of the proxy
-  await hre.ethers.getContractAt("TransparentUpgradeableProxy", proxy.address)
+  await hre.ethers.getContractAt("Proxy", proxy.address)
     .then((proxyContract) => proxyContract.changeAdmin(proxyAdmin.address));
   console.log("Proxy admin set to:", proxyAdmin.address);
 }
