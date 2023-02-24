b12e96b8b85c8aa7b66b67a14631760be05013d0
Danish Ali <69537135+Danishlynx@users.noreply.github.com>
2023-02-24 15:27:02 +0800

    Update Proxy.sol

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -1,18 +1,22 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
-abstract contract Proxy is Initializable {
-    function implementation() public view virtual returns (address);
-    function upgradeTo(address _impl) public virtual;
-    function upgradeToAndCall(address _impl, bytes memory _data) public payable virtual;
+contract Proxy is TransparentUpgradeableProxy {
+
+    constructor(address _logic, address _admin, bytes memory _data) payable TransparentUpgradeableProxy(_logic, _admin, _data) {
+    }
     
-    fallback() external payable {
+    function implementation() public view override returns (address) {
+        return super.implementation();
+    }
+    
+    fallback() virtual external payable {
         revert("Fallback function not implemented.");
     }
     
-    receive() external payable {
+    receive() virtual external payable {
         revert("Receive function not implemented.");
     }
 }
