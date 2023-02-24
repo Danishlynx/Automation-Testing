2ffe6ad0bd929f54a5ebd7651ea4612f538890e1
Danish Ali <69537135+Danishlynx@users.noreply.github.com>
2023-02-24 15:33:53 +0800

    Update deploy_Proxy_proxyAdmin.js

--- scripts/deploy_Proxy_proxyAdmin.js
+++ scripts/deploy_Proxy_proxyAdmin.js
@@ -1,6 +1,5 @@
 const { ethers } = require("hardhat");
-const { TransparentUpgradeableProxy } = require('@openzeppelin/contracts-upgradeable/proxy/transparent/TransparentUpgradeableProxy');
-const { TransparentUpgradeableProxyAdmin } = require('@openzeppelin/contracts-upgradeable/proxy/transparent/TransparentUpgradeableProxyAdmin');
+
 
 async function main() {
   const proxyAdmin = await ethers.getContractFactory("ProxyAdmin");
@@ -10,7 +9,7 @@ async function main() {
 
   const originalAddress = "0x8BDA3Aee6f7835dac51ec28F672cbD718EBB5A0F"; // replace with your deployed Original contract address
 
-  const proxy = await ethers.getContractFactory("TransparentUpgradeableProxy");
+  const proxy = await ethers.getContractFactory("Proxy");
   const proxyInstance = await proxy.deploy(
     originalAddress,
     adminInstance.address,
