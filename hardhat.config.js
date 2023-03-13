6527f8873eb1d701042eb584c3fbeeb09d2862ef
Danish Ali <69537135+Danishlynx@users.noreply.github.com>
2023-03-13 14:48:34 +0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,22 +4,6 @@ require("dotenv").config()
 require('@openzeppelin/hardhat-upgrades');
 
 
-
-
-//require('dotenv').config(); 
-const deployKey = "b7059a1f8aacf38cba35a2075d6df5b52c5f08786c69d80ae307466a7e9f1cbc";
-
-// Go to https://www.alchemyapi.io, sign up, create
-// a new App in its dashboard, and replace "KEY" with its key
-const ALCHEMY_API_KEY = "fTj-d_vmXZ_YFh9JfH1ZCrBmd_kH--Vq";
-
-// Replace this private key with your Goerli account private key
-// To export your private key from Metamask, open Metamask and
-// go to Account Details > Export Private Key
-// Beware: NEVER put real Ether into testing accounts
-const GOERLI_PRIVATE_KEY = "e51acde034d4294589ec534d0c223a1d87e5a50857658e0194364a6b5d746f29";
-
-
 module.exports = {
   solidity: "0.8.9",
   networks: {
@@ -29,8 +13,8 @@ module.exports = {
     },
     volta: {
       url: "https://volta-rpc.energyweb.org",
-      accounts: [deployKey],
-      chainId: 73799
+      chainId: 73799,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
     ewc: {
       url: "https://rpc.energyweb.org",
