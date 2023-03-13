6b4978c588ed93138bb85a15a72f3442fa6ce6d6
Danish Ali <69537135+Danishlynx@users.noreply.github.com>
2023-03-13 15:01:12 +0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,27 +1,42 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config()
 
-require('@openzeppelin/hardhat-upgrades');
-
+/** @type import('hardhat/config').HardhatUserConfig */
 
 module.exports = {
-  solidity: "0.8.9",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.17",
+      },
+    ],
+  },
   networks: {
-    goerli: {
-      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-      accounts: [GOERLI_PRIVATE_KEY]
+    ganache: {
+      url: "http://localhost:8545",
     },
     volta: {
       url: "https://volta-rpc.energyweb.org",
       chainId: 73799,
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
-    ewc: {
-      url: "https://rpc.energyweb.org",
-      chainId: 246,
-      accounts: [deployKey]
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     }
-   
-      
-    }
-  };
+  },
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+  },
+  gasReporter: {
+    currency: 'USD',
+    gasPrice: 21,
+    enabled: false,
+    coinmarketcap: process.env.COIN_MARKET_CAP_API,
+    token: "EWT",
+    enabled: process.env.GAS_REPORTING == "activate" ?  true : false
+  },
+};
